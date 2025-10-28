b9d4b2a82a937f31ac82c4a4a495c3db2c1d11ba
Jan Nielek <nielu92@gmail.com>
2025-10-28 15:37:00 +0100

    Initial commit

---
+++ addons/DefenseInDepth/Scripts/Game/DiD/modded/AI/Reaction/Danger/SCR_AIDangerReaction_WeaponFired.c
@@ -0,0 +1,6 @@
+[BaseContainerProps()]
+modded class SCR_AIDangerReaction_WeaponFired : SCR_AIDangerReaction
+{
+	protected static const float AUDIBLE_DISTANCE_NORMAL = 125.0; // Audible distance for normal gunshots
+	protected static const float AUDIBLE_DISTANCE_SUPPRESSED = 50.0; // Audible distance for suppressed gunshots
+};
